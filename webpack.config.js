46bde7e5129decb7693c416f658abe257e152ed0
Max <mniamek9@gmail.com>
2022-02-05 11:55:45 +0100

    Cleaning up the workspace

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  context: path.resolve(__dirname, "assets"),
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "assets/dist"),
+  },
+  watch: true,
+};
